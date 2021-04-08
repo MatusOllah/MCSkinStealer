b4d6911d1228d30100737a4bf948c8527f5c908f
MatusOllah <57183251+MatusOllah@users.noreply.github.com>
2021-04-08 08:08:40 +0200

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,10 @@
+module MCSkinStealer
+
+go 1.14
+
+require (
+	github.com/VladimirMarkelov/clui v1.2.1
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/nsf/termbox-go v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"MCSkinStealer/downloader"
+
+	ui "github.com/VladimirMarkelov/clui"
+)
+
+func CreateForm() {
+	form := ui.AddWindow(0, 0, 40, 10, "Stiahnuť Minecraft skin")
+
+	form.SetPack(ui.Vertical)
+	form.SetPaddings(2, 2)
+	form.SetGaps(ui.AutoSize, 1)
+	form.SetActiveBackColor(ui.ColorGreen)
+	form.SetActiveTextColor(ui.ColorWhite)
+	form.SetBackColor(ui.ColorBlue)
+	form.SetBorder(ui.BorderThick)
+
+	frmPlayer := ui.CreateFrame(form, ui.AutoSize, 1, ui.BorderNone, 1)
+	frmPlayer.SetActiveBackColor(ui.ColorGreen)
+	frmPlayer.SetActiveTextColor(ui.ColorWhite)
+	frmPlayer.SetBackColor(ui.ColorBlue)
+	ui.CreateLabel(frmPlayer, ui.AutoSize, ui.AutoSize, "Hráč: ", 1)
+	playerInput := ui.CreateEditField(frmPlayer, ui.AutoSize, "", 1)
+
+	frmControl := ui.CreateFrame(form, ui.AutoSize, 4, ui.BorderNone, 1)
+	frmControl.SetActiveBackColor(ui.ColorGreen)
+	frmControl.SetActiveTextColor(ui.ColorWhite)
+	frmControl.SetBackColor(ui.ColorBlue)
+	btnSkin := ui.CreateButton(frmControl, ui.AutoSize, 4, "Stiahnuť skin", 1)
+	btnBody := ui.CreateButton(frmControl, ui.AutoSize, 4, "Stiahnuť telo", 1)
+	btnQuit := ui.CreateButton(frmControl, ui.AutoSize, 4, "Koniec", 1)
+
+	btnQuit.OnClick(func(ev ui.Event) {
+		go ui.Stop()
+	})
+
+	btnSkin.OnClick(func(ev ui.Event) {
+		fileUrl := "https://minotar.net/skin/" + playerInput.Title()
+
+		err := downloader.DownloadFile("skin_"+playerInput.Title()+".png", fileUrl)
+
+		if err != nil {
+			ui.CreateAlertDialog("Chyba", "Chyba sťahovania súboru.", "OK")
+		}
+	})
+
+	btnBody.OnClick(func(ev ui.Event) {
+		fileUrl := "https://minotar.net/body/" + playerInput.Title() + "/100.png"
+		err := downloader.DownloadFile("body_"+playerInput.Title()+".png", fileUrl)
+
+		if err != nil {
+			ui.CreateAlertDialog("Chyba", "Chyba sťahovania súboru.", "OK")
+		}
+	})
+
+}
+
+func main() {
+	ui.InitLibrary()
+
+	defer ui.DeinitLibrary()
+
+	CreateForm()
+
+	ui.MainLoop()
+}
